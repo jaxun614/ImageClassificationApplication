3511791a7d1de5c802a1c0277275a90e547be27d
Jackson Adams <jacksonadams614@gmail.com>
2024-04-28 23:40:54 -0700

    added to the Image class

--- ImageClassificationApplication/src/Image.java
+++ ImageClassificationApplication/src/Image.java
@@ -1,7 +1,15 @@
 
 // This class is responsible for the images
-// It will 
+// It will create a new image object
 public class Image 
 {
+	private int imageNumber;
+	private static int imageCount = 0;
+	private 
+	
+	public Image(Image image, Information information)
+	{
+
+	}
 
 }
