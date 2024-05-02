3768ae8d4a9e70bb13b95782a57fc6ecc95f85dd
Jackson Adams <jacksonadams614@gmail.com>
2024-05-02 10:14:21 -0700

    added a class for searching
    Coded the class to search for images

--- ImageClassificationApplication/src/ImageSearchEngine.java
+++ ImageClassificationApplication/src/ImageSearchEngine.java
@@ -1,4 +1,21 @@
+import java.util.List;
 
-public class ImageSearchEngine {
-
+public class ImageSearchEngine 
+{
+	private Database database;
+	
+	public ImageSearchEngine(Database database)
+	{
+		this.database = database;
+	}
+	
+	public List<Image> searchByKeyword(String keyword)
+	{
+		return database.searchByKeyword(keyword);
+	}
+	
+	public List<Image> searchBySimilarity(Image queryImage)
+	{
+		return database.searchBySimilarity(queryImage);
+	}
 }
