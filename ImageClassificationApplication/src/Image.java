bfbf89ab642821ff4cda0b3c5ff9d883b79a72d0
Jackson Adams <jacksonadams614@gmail.com>
2024-05-02 10:08:56 -0700

    class code added
    1. wrote the code for the Image class
    2. deleted old classes that need to be rewriten

--- ImageClassificationApplication/src/Image.java
+++ ImageClassificationApplication/src/Image.java
@@ -1,15 +1,41 @@
 
 // This class is responsible for the images
 // It will create a new image object
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Image 
 {
-	private int imageNumber;
-	private static int imageCount = 0;
-	private 
+	private String filePath;
+	private Map<String, String> metadata;
 	
-	public Image(Image image, Information information)
+	public Image(String filePath, Map<String, String> metadata)
 	{
-
+		this.filePath = filePath;
+		this.metadata = metadata != null ? metadata: new HashMap<>();
+	}
+	
+	// gets the file path of the image
+	public String getFilePath()
+	{
+		return filePath;
+	}
+	
+	// gets the metadata of the image
+	public Map<String, String> getMetadata() 
+	{
+		return metadata;
+	}
+	
+	public void setMetadata(String key, String value)
+	{
+		metadata.put(key,  value);
+	}
+	
+	public void upload()
+	{
+		// TODO : code for uploading the image
 	}
 
 }
