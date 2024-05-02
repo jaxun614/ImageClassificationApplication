597c14d94c1068125137df35f014e77da7491876
Jackson Adams <jacksonadams614@gmail.com>
2024-05-02 10:23:46 -0700

    Added the database code
    this class creates a database that images can be stored in

--- ImageClassificationApplication/src/Database.java
+++ ImageClassificationApplication/src/Database.java
@@ -1,4 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
 
-public class Database {
-
+public class Database 
+{
+	private List<Image> images;
+	
+	public Database()
+	{
+		this.images = new ArrayList<>();
+	}
+	
+	public void addImage(Image image)
+	{
+		images.add(image);
+	}
+	
+	public List<Image> getAllImages()
+	{
+		return images;
+	}
+	
+	public List<Image> searchByKeyword(String keyword)
+	{
+		List<Image> results = new ArrayList<>();
+		for(Image image : images)
+		{
+			if(image.getMetadata().containsValue(keyword))
+			{
+				results.add(image);
+			}
+		}
+		return results;
+	}
+	
+	public List<Image> searchBySimilarity(Image queryImage)
+	{
+		return new ArrayList<>();
+	}
 }
